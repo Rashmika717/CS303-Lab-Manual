170138da34dc81da33948c802feea2a6198938e4
Rashmika717 <rashmikasingh717@gmail.com>
2025-12-23 02:22:50 +0530

    Update B4Prg.c

--- Section-B(Stack)/B4Prg.c
+++ Section-B(Stack)/B4Prg.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int factorial(int n) {
+  // Base condition
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    else {
+      // Recursive call
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d\n", num, factorial(num));
+
+    return 0;
+}
